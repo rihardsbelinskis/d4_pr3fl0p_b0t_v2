9641b793ec0a903d92bb933ff9069dd7ad0515c4
root <rihards.belinskis@gmail.com>
2020-04-11 17:34:38 +0200

    Added img folder and edited preflopranges.py

--- preflopranges.py
+++ preflopranges.py
@@ -14,14 +14,14 @@ CO_RFI = ('AAo','AKs','AQs','AJs','ATs','A9s','A8s','A7s','A6s','A5s','A4s','A3s
           'T8s','99o','98s','97s','88o','87s','86s','77o','76s','75s','66o','65s','55o','54s',
           '44o','33o','22o')
 
-BTN_RFI ('AAo','AKs','AQs','AJs','ATs','A9s','A8s','A7s','A6s','A5s','A4s','A3s','A2s','AKo',
+BTN_RFI = ('AAo','AKs','AQs','AJs','ATs','A9s','A8s','A7s','A6s','A5s','A4s','A3s','A2s','AKo',
          'KKo','KQs','KJs','KTs','K9s','K8s','K7s','K6s','K5s','K4s','AQo','KQo','QQo','QJs',
          'QTs','Q9s','Q8s','Q7s','Q6s','AJo','KJo','QJo','JJo','JTs','J9s','J8s','J7s','ATo',
          'KTo','QTo','JTo','TTo', 'T9s','T8s','T7s','A9o','K9o','Q9o','J9o','T9o','99o','98s',
          '97s','96s','A8o','88o','87s','86s','85s','A7o','77o','76s','75s','A6o','66o','65s',
          '64s','A5o','55o','54s','53s','A4o','44o','43s','33o','22o')
 
-SB_RFI ('AAo','AKs','AQs','AJs','ATs','A9s','A8s','A7s','A6s','A5s','A4s','A3s','A2s','AKo',
+SB_RFI = ('AAo','AKs','AQs','AJs','ATs','A9s','A8s','A7s','A6s','A5s','A4s','A3s','A2s','AKo',
         'KKo','KQs','KJs','KTs','K9s','K8s','K7s','K6s','K5s','K4s','K3s','AQo','KQo','QQo',
         'QJs','QTs','Q9s','Q8s','Q7s','Q6s','Q5s','AJo','KJo','QJo','JJo','JTs','J9s','J8s',
         'J7s','J6s','ATo','KTo','QTo','JTo','TTo', 'T9s','T8s','T7s','T6s','A9o','K9o','Q9o',
@@ -29,7 +29,7 @@ SB_RFI ('AAo','AKs','AQs','AJs','ATs','A9s','A8s','A7s','A6s','A5s','A4s','A3s',
         '77o','76s','75s','A6o','66o','65s','64s','A5o','55o','54s','53s','A4o','44o','43s',
         'A3o','33o','A2o','22o')
 
-BB_RFI ('AAo','AKs','AQs','AJs','ATs','A9s','A8s','A7s','A6s','A5s','A4s','A3s','A2s','AKo',
+BB_RFI = ('AAo','AKs','AQs','AJs','ATs','A9s','A8s','A7s','A6s','A5s','A4s','A3s','A2s','AKo',
         'KKo','KQs','KJs','KTs','K9s','K8s','K7s','K6s','AQo','KQo','QQo','QJs','QTs','Q9s',
         'Q8s','Q7s','Q6s','Q5s','AJo','KJo','QJo','JJo','JTs','J9s','J8s','J7s','ATo','KTo',
         'QTo','JTo','TTo', 'T9s','T8s','A9o','K9o','Q9o','J9o','T9o','99o','98s','97s','A8o',
